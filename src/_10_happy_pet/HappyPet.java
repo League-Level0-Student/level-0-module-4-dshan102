558a037fefd678030e8ec20440248a4ba654c4fb
League Student <league@iMac-17.attlocal.net>
2019-12-01 15:58:57 -0800

    own adventure

--- src/_10_happy_pet/HappyPet.java
+++ src/_10_happy_pet/HappyPet.java
@@ -4,27 +4,58 @@ import javax.swing.JOptionPane;
 public class HappyPet {
 	
 	// 2. Add the following variable to the next line: static int happinessLevel = 0;
+	static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
 	
 	public static void main(String[] args) {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
+		String pet = JOptionPane.showInputDialog("What pet would you like to buy?");
 
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
-		
+		for (int i=0; i<10; i++) {
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
+			int task = JOptionPane.showOptionDialog(null, "What would you like to do with your pet?", "HappyPet", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "Feed", "Walk", "Clean" }, null);
 
 			// 5. Use user input to call the appropriate method created in step 4.
+			if (task == 0) {
+				feed();
+			}
+			else if (task == 1){
+				walk();
+			}
+			else if (task == 2) {
+				clean();
+			}
+			System.out.println(happinessLevel);
+			if (happinessLevel == 7) {
+				JOptionPane.showMessageDialog(null, "You love your pet.");
+				break;
+			}
+		}
 
 			// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
+		
 
 	}
 
-	// 4. Create methods to handle each of your user selections.
+	// 4. Create methods to handle each of your user selections. 
+		public static void feed() {
+			JOptionPane.showMessageDialog(null, "Your pet enjoys its food.");
+			happinessLevel+=1;
+		}
+		public static void walk() {
+			JOptionPane.showMessageDialog(null, "Your pet enjoys its walk.");
+			happinessLevel+=1;
+		}
+		public static void clean() {
+			JOptionPane.showMessageDialog(null, "Your pet hates its cleaning.");
+			happinessLevel-=1;
+		}
+		
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
 }
---
+++ src/_11_own_adventure/Own_Adventure.java
@@ -0,0 +1,9 @@
+package _11_own_adventure;
+
+import javax.swing.JOptionPane;
+
+public class Own_Adventure {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null, "Your name is ");
+}
+}
